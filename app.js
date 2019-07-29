6283cd6dba7dda48685785ec9b096572076339b7
Matthew McGuff <matthew.mcguff@walmart.com>
2019-07-29 08:01:17 -0500

    amazon script with rp without proxy

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const amazon = require('./routes/amazon');
 const urbanOutfitters = require('./routes/urban-outfitters');
 
 (async () =>{
-    //await reddit.init();
-    await amazon.init();
-    
-    //close all browsers to avoid memory leaks when converting data create an API out of the scraped data. 
-    //await reddit.close(); 
-    await amazon.close();
+    console.log('Reddit scrape result:');
+    await reddit.init();
+    await reddit.close(); 
+
+    console.log('Amazon scrape result:')
+    await amazon.scrape('https://www.amazon.com/gp/product/B01DXI33T0');
     //Very helpful for seeing that the objects collected from your scrap are what you expect. 
     //debugger;  
 })();
--- routes/amazon.js
+++ routes/amazon.js
@@ -1,45 +1,25 @@
-const puppeteer = require('puppeteer');
+const rp = require('request-promise');
+const cheerio = require('cheerio'); // Basically jQuery for node.js
+ 
+internals = {};
 
-const targetUrl = 'https://www.amazon.com/gp/product/B01DXI33T0'; 
-const pc = {
-    username: '108.59.3.140',
-    password: '18015',
-    host: '165.227.118.92',
-    port: '8002'
-}
-//const proxy =  `https://${pc.username}:${pc.password}@${pc.host}:${pc.port}`;
+internals.scrape = (targetUrl) => {
+    const options = {
+        uri: targetUrl,
+        transform: function (body) {
+            return cheerio.load(body);
+        }
+    };
+     
+    rp(options)
+        .then(function ($) {
+            console.log($.html());
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+  };
 
-let html = '';
 
-const self = {
-    browser: null,
-    page: null,
 
-    init: async() => {
-        self.browser = await puppeteer.launch({
-            args: [
-            //'--proxy-server=' + proxy,
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-            '--disable-accelerated-2d-canvas',
-            '--disable-gpu',
-            '--window-size=1920x1080',
-            ],
-          });
-        
-        self.page = await self.browser.newPage();
-        await self.page.goto(targetUrl, {waituntil: 'networkidle0'});
-
-        //most basic implemenation
-        html = await self.page.content();
-        console.log(html);
-
-        //transformation to get the desired elements would go here
-    },
-    close: async () => {
-        await self.browser.close();
-    }
-}
-
-module.exports = self;
+module.exports = internals;
