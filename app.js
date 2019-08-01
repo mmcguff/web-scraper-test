a41cf13e0e85fc32fe84679d71c8c761dc6e35a1
Matthew McGuff <matthew.mcguff@walmart.com>
2019-08-01 07:55:48 -0500

    testing something.

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const urbanOutfitters = require('./routes/urban-outfitters');
 
 (async () =>{
     
-    await reddit.scrape('https://old.reddit.com');
-    await amazon.scrape('https://www.amazon.com/gp/product/B01DXI33T0');
-    await urbanOutfitters.scrape('https://www.urbanoutfitters.com/shop/uo-corduroy-hooded-cropped-jacket?category=jackets-coats-for-women&color=081&type=REGULAR');
+    //await reddit.scrape('https://old.reddit.com');
+    //await amazon.scrape('https://www.google.com');
+    await amazon.scrape(' https://www.amazon.com/gp/product/B01DXI33T0');
+    //await urbanOutfitters.scrape('https://www.urbanoutfitters.com/shop/uo-corduroy-hooded-cropped-jacket?category=jackets-coats-for-women&color=081&type=REGULAR');
     //debugger; 
 })();
--- routes/amazon.js
+++ routes/amazon.js
@@ -1,11 +1,21 @@
 const rp = require('request-promise');
 const cheerio = require('cheerio'); // Basically jQuery for node.js
- 
+const url = require('url');
+const pc = {
+    username: '108.59.3.140',
+    password: '18015',
+    host: '165.227.118.92',
+    port: '8002'
+}               
+const proxy =  'http://108.59.3.140:18015@165.227.118.92:8002';
+
+
 internals = {};
 
-internals.scrape = (targetUrl) => {
+internals.scrape = async (targetUrl) => {
+
     const options = {
-        uri: targetUrl,
+        uri: proxy + targetUrl,
         transform: function (body) {
             return cheerio.load(body);
         }
